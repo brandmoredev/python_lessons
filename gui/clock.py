a35bff392fa152299b649669b52de5f4da401f99
brandmoredev <brandmoredev@gmail.com>
2024-08-16 21:42:28 +0300

    Updates clock

--- gui/clock.py
+++ gui/clock.py
@@ -6,10 +6,16 @@ def update():
     time_label.config(text=time_string)
     time_label.after(1000, update)
 
+    date_string = strftime("%B %d, %Y")
+    date_label.config(text=date_string)
+
 window = Tk()
 
 time_label = Label(window, font=("Arial", 50), fg="#00FF00", bg="black")
 time_label.pack()
+
+date_label = Label(window, font=("Arial", 50), fg="#000000")
+date_label.pack()
 
 update()
 
